28662cb1b2446c797d274a243727b7971aee6207
Anton <dev@atjn.dk>
2026-05-11 13:08:07 +0200

    Fix form URL handling

--- src/main/resources/static/js/site.js
+++ src/main/resources/static/js/site.js
@@ -1,10 +1,9 @@
 import "htmx.org";
 import "htmx-ext-head-support";
 
-const form = document.getElementById("filter-form");
-
-form?.addEventListener("htmx:afterRequest", () => {
+document.addEventListener("htmx:afterRequest", (event) => {
+  if(!(event.target instanceof HTMLFormElement)) return;
   const url = new URL(window.location.href);
-  url.search = new URLSearchParams(new FormData(form)).toString();
+  url.search = new URLSearchParams(new FormData(event.target)).toString();
   history.replaceState({}, "", url);
 });
